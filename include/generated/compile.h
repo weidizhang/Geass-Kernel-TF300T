d2ef61fd729bec731c08bcfcf55217eb9edca402
root <root@ebil.(none)>
2014-02-01 20:24:33 -0500

    Add interactiveX governor, add ramdisk file with init.d support added to github

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Sun Jan 26 14:23:11 EST 2014"
+#define UTS_VERSION "#5 SMP PREEMPT Sat Feb 1 20:01:15 EST 2014"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "ebil"
 #define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.1.10-g081d5ba-dirty"
+#define UTS_RELEASE "3.1.10-g6e570f1-dirty"
